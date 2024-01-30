f4ba1bd699f740e6cdfb7d92bd2926fc31695a06
Mattia DEIDDA <mattia.deidda@alten.it>
2024-01-30 18:33:37 +0100

    Add project files.

---
+++ GiocoCoppie/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace GiocoCoppie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutCoppie = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.timerStart = new System.Windows.Forms.Timer(this.components);
+            this.timerShow = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutCoppie.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutCoppie
+            // 
+            this.tableLayoutCoppie.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutCoppie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tableLayoutCoppie.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutCoppie.ColumnCount = 4;
+            this.tableLayoutCoppie.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutCoppie.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutCoppie.Controls.Add(this.panel3, 2, 0);
+            this.tableLayoutCoppie.Controls.Add(this.panel4, 3, 0);
+            this.tableLayoutCoppie.Controls.Add(this.panel5, 0, 1);
+            this.tableLayoutCoppie.Controls.Add(this.panel6, 1, 1);
+            this.tableLayoutCoppie.Controls.Add(this.panel7, 2, 1);
+            this.tableLayoutCoppie.Controls.Add(this.panel8, 3, 1);
+            this.tableLayoutCoppie.Controls.Add(this.panel9, 0, 2);
+            this.tableLayoutCoppie.Controls.Add(this.panel10, 0, 3);
+            this.tableLayoutCoppie.Controls.Add(this.panel11, 1, 2);
+            this.tableLayoutCoppie.Controls.Add(this.panel12, 1, 3);
+            this.tableLayoutCoppie.Controls.Add(this.panel13, 2, 2);
+            this.tableLayoutCoppie.Controls.Add(this.panel14, 3, 2);
+            this.tableLayoutCoppie.Controls.Add(this.panel15, 2, 3);
+            this.tableLayoutCoppie.Controls.Add(this.panel16, 3, 3);
+            this.tableLayoutCoppie.Location = new System.Drawing.Point(-1, 2);
+            this.tableLayoutCoppie.Name = "tableLayoutCoppie";
+            this.tableLayoutCoppie.RowCount = 4;
+            this.tableLayoutCoppie.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutCoppie.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutCoppie.Size = new System.Drawing.Size(628, 336);
+            this.tableLayoutCoppie.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(147, 74);
+            this.panel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(162, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(147, 74);
+            this.panel2.TabIndex = 17;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(318, 6);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(147, 74);
+            this.panel3.TabIndex = 18;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(474, 6);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(148, 74);
+            this.panel4.TabIndex = 19;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(6, 89);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(147, 74);
+            this.panel5.TabIndex = 20;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(162, 89);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(147, 74);
+            this.panel6.TabIndex = 21;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(318, 89);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(147, 74);
+            this.panel7.TabIndex = 22;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(474, 89);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(148, 74);
+            this.panel8.TabIndex = 23;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(6, 172);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(147, 74);
+            this.panel9.TabIndex = 24;
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(6, 255);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(147, 75);
+            this.panel10.TabIndex = 25;
+            // 
+            // panel11
+            // 
+            this.panel11.Location = new System.Drawing.Point(162, 172);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(147, 74);
+            this.panel11.TabIndex = 26;
+            // 
+            // panel12
+            // 
+            this.panel12.Location = new System.Drawing.Point(162, 255);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(147, 75);
+            this.panel12.TabIndex = 27;
+            // 
+            // panel13
+            // 
+            this.panel13.Location = new System.Drawing.Point(318, 172);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(147, 74);
+            this.panel13.TabIndex = 28;
+            // 
+            // panel14
+            // 
+            this.panel14.Location = new System.Drawing.Point(474, 172);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(148, 74);
+            this.panel14.TabIndex = 29;
+            // 
+            // panel15
+            // 
+            this.panel15.Location = new System.Drawing.Point(318, 255);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(147, 75);
+            this.panel15.TabIndex = 30;
+            // 
+            // panel16
+            // 
+            this.panel16.Location = new System.Drawing.Point(474, 255);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(148, 75);
+            this.panel16.TabIndex = 31;
+            // 
+            // timerStart
+            // 
+            this.timerStart.Interval = 10000;
+            this.timerStart.Tick += new System.EventHandler(this.timerStart_Tick);
+            // 
+            // timerShow
+            // 
+            this.timerShow.Interval = 650;
+            this.timerShow.Tick += new System.EventHandler(this.timerShow_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 338);
+            this.Controls.Add(this.tableLayoutCoppie);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gioco delle Coppie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutCoppie.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutCoppie;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Timer timerStart;
+        private System.Windows.Forms.Timer timerShow;
+    }
+}
+
---
+++ GiocoCoppie/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GiocoCoppie
+{
+    public partial class Form1 : Form
+    {
+        List<string> images;
+        Random random;
+        string path = "";
+        int firstClick = 0;
+        int errors = 0;
+        int points = 0;
+        int totalPairs = 8;
+        int maxError = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void label_MouseEnter(object sender, EventArgs e)
+        {
+            Label box = (Label)sender;
+            box.BackColor = Color.BurlyWood;
+            box.Cursor = Cursors.Hand;
+        }
+
+        private void label_MouseLeave(object sender, EventArgs e)
+        {
+            Label box = (Label)sender;
+            box.BackColor = Color.FromArgb(255, 232, 232);
+            box.Cursor = Cursors.Default;
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            label.Visible = false;
+
+            Panel p=label.Parent as Panel;
+
+            PictureBox box = GetPictureBox(p);
+
+            string img = box.ImageLocation;
+
+            if (firstClick == 0)
+            {
+                path = img;
+                firstClick++;
+            }
+            else
+            {
+                if (path!=img)
+                {
+                    timerShow.Start();
+                    errors++;
+                    points = 0;
+
+                    if (errors == maxError)
+                    {
+                        MessageBox.Show("Game Over!!", "FAIL");
+
+                        ClearGame();
+
+                        StartGame();
+                    }
+                }
+                else
+                {
+                    points++;
+                }
+
+                firstClick = 0;
+            }
+
+            if (points == totalPairs)
+            {
+                MessageBox.Show("You Win!!", "SUCCESS");
+
+                ClearGame();
+
+                StartGame();
+            }
+            
+        }
+
+        private void timerStart_Tick(object sender, EventArgs e)
+        {
+            HideCards();
+
+            timerStart.Stop();
+        }
+
+        private void timerShow_Tick(object sender, EventArgs e)
+        {
+            HideCards();
+            timerShow.Stop();
+        }
+
+        public void HideCards()
+        {
+            foreach (Panel panel in tableLayoutCoppie.Controls)
+            {
+                foreach (Control c in panel.Controls)
+                {
+                    Label label = c as Label;
+
+                    if (label != null)
+                    {
+                        label.Visible = true;
+                    }
+                }
+            }
+        }
+
+        public PictureBox GetPictureBox(Panel p)
+        {
+            return p.Controls.OfType<PictureBox>().FirstOrDefault();
+        }    
+
+        public void StartGame()
+        {
+            var basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            images = new List<string>()
+            {
+                 Path.Combine(basePath, "images","ananas.png"),
+                 Path.Combine(basePath, "images","arancio.png"),
+                 Path.Combine(basePath, "images","banana.png"),
+                 Path.Combine(basePath, "images","ciliegia.png"),
+                 Path.Combine(basePath, "images","dino.png"),
+                 Path.Combine(basePath, "images","fragola.png"),
+                 Path.Combine(basePath, "images", "giraffa.png"),
+                 Path.Combine(basePath, "images","mela.png"),
+
+                 Path.Combine(basePath, "images","ananas.png"),
+                 Path.Combine(basePath, "images","arancio.png"),
+                 Path.Combine(basePath, "images","banana.png"),
+                 Path.Combine(basePath, "images","ciliegia.png"),
+                 Path.Combine(basePath, "images","dino.png"),
+                 Path.Combine(basePath, "images","fragola.png"),
+                 Path.Combine(basePath, "images", "giraffa.png"),
+                 Path.Combine(basePath, "images","mela.png")
+            };
+
+            foreach (Panel panel in tableLayoutCoppie.Controls)
+            {
+                Label label = new Label();
+                PictureBox box = new PictureBox();
+
+                box.Dock = DockStyle.Fill;
+
+                int indice = random.Next(images.Count);
+                string img = images.ElementAt(indice);
+
+                box.ImageLocation = img;
+                box.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                images.RemoveAt(indice);
+
+                label.Dock = DockStyle.Fill;
+                label.Visible = false;
+                label.BackColor = Color.FromArgb(255, 232, 232);
+
+                label.MouseEnter += new EventHandler(label_MouseEnter);
+                label.MouseLeave += new EventHandler(label_MouseLeave);
+                label.Click += new EventHandler(label_Click);
+
+                panel.Controls.Add(label);
+                panel.Controls.Add(box);
+
+            }
+
+            timerStart.Start();
+        }
+
+        public void ClearGame()
+        {
+            foreach (Control control in tableLayoutCoppie.Controls)
+            {
+                if (control is Panel panel)
+                {
+                    panel.Controls.Clear();
+                }
+            }
+
+            errors = 0;
+            points = 0;
+        }
+
+
+    }
+}
